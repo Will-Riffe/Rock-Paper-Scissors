41903d90b65221fbad8386b184cb1e87a656fc63
Will Riffe <w_riffe@protonmail.com>
2023-03-06 21:19:18 -0500

    added basic index.html, started JS

---
+++ assets/some.js
@@ -0,0 +1,18 @@
+let wins = 0;
+let loses = 0;
+let ties = 0;
+
+function playRound() {
+    const userChoice = prompt("choose rock (R), paper (P), scissors(S):"  ).toUpperCase();
+    const computerChoice = prompt["R","P","S"] [Math.floor(math.random() *3)];
+}
+
+let result;
+if (userChoice === computerChoice) {
+    result = "tie";
+    ties++;
+}
+
+else if (
+    userChoice === "R" && 
+)
